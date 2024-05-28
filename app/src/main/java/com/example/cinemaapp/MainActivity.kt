cc26f3ab7cdd5311895eb099a824da97e2a73c08
Jambo4231 <jsim1992@hotmail.co.uk>
2024-05-28 16:29:14 +0100

    Working

--- app/src/main/java/com/example/cinemaapp/MainActivity.kt
+++ app/src/main/java/com/example/cinemaapp/MainActivity.kt
@@ -1,29 +1,43 @@
-package com.example.cinema_assignment
+package com.example.cinemaapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.cinema_assignment.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        setupActionBarWithNavController(navController)
-        binding.bottomNavigationView.setupWithNavController(navController)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_films -> {
+                    loadFragment(FilmsFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_account -> {
+                    loadFragment(AccountFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.navigation_map -> {
+                    loadFragment(MapFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+        // Set default selected item
+        bottomNavigationView.selectedItemId = R.id.navigation_films
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .commit()
     }
 }
