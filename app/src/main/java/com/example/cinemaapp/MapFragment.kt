9763de975c8dc81ccba3fe42c169b77afec61af5
Jambo4231 <jsim1992@hotmail.co.uk>
2024-05-28 23:03:26 +0100

    film details + map working

--- app/src/main/java/com/example/cinemaapp/MapFragment.kt
+++ app/src/main/java/com/example/cinemaapp/MapFragment.kt
@@ -1,8 +1,27 @@
 package com.example.cinemaapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : Fragment(R.layout.fragment_map) {
-    // Implement map functionality here
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        // Add a marker in a default location and move the camera with a zoom level
+        val cinemaLocation = LatLng(56.819817, -5.105218)
+        googleMap.addMarker(MarkerOptions().position(cinemaLocation).title("Cinema Location"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cinemaLocation, 15f))
+    }
 }
